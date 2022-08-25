cff3d122faf4e039fcdcfaf0ec2e011c830e2d3c
Nicolas Tribukait <Nio-av@users.noreply.github.com>
2022-08-25 16:36:04 +0200

    include Landing Page

---
+++ www/html/api.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../vendor/autoload.php';
+
+
+use App\View\ApiEndpoints\DemoEndpoint;
+
+
+
+
+$homeController = new DemoEndpoint();
+if (key_exists('type', $_GET)) {
+    $type = $_GET['type'];
+    $homeController->renderAction($type);
+} else {
+    $homeController->renderAction();
+}
--- www/html/index.php
+++ www/html/index.php
@@ -1,17 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="de">
 
-require '../vendor/autoload.php';
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="/script.js"></script>
+  <link rel="stylesheet" type="text/css" href="/style.css">
+  <title>Shop Tools</title>
+</head>
 
+<body>
+  <h1>Shop Tools</h1>
+  <h2>Entry Point for Shop Tools</h2>
+  <ul>
+    <li><a href="/api.php?type=Json">Json</a></li>
+    <li><a href="/api.php?type=Xml">Xml</a></li>
+    <li><a href="/api.php?type=Csv">CSV</a></li>
+  </ul>
+  <a href=" //localhost:8080">PHP My Admin</a>
+  <?php
+  phpinfo();
+  ?>
+</body>
 
-use App\View\ApiEndpoints\DemoEndpoint;
-
-
-
-
-$homeController = new DemoEndpoint();
-if (key_exists('type', $_GET)) {
-    $type = $_GET['type'];
-    $homeController->renderAction($type);
-} else {
-    $homeController->renderAction();
-}
+</html>
