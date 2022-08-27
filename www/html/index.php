b27454b4229b32cb730f55ec85d4abcdac9ed836
Nicolas Tribukait <Nio-av@users.noreply.github.com>
2022-08-27 22:01:14 +0200

    prepare for test

--- www/html/index.php
+++ www/html/index.php
@@ -19,7 +19,7 @@
   </ul>
   <h2>Test Action</h2>
   <a href="/testView.php">Test View</a>
-  <a href=" //localhost:8080">PHP My Admin</a>
+  <!-- <a href=" //localhost:8080">PHP My Admin</a> -->
   <?php
   phpinfo();
   ?>
